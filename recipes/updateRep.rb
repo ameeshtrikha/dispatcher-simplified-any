9a10f5150f9f538e74c01054d154cb8db85b1bca
Ameesh Trikha <ameesh.trikha@gmail.com>
2016-03-28 19:00:31 +0000

    updateRep.rb edited online with Bitbucket

--- recipes/updateRep.rb
+++ recipes/updateRep.rb
@@ -1,10 +1,8 @@
-if node[:platform_family].include?("rhel")
-    bash 'update_for_apache_2.4' do
-      user 'root'
-      cwd '/etc/yum.repos.d/'
-      code <<-EOH
-      wget http://repos.fedorapeople.org/repos/jkaluza/httpd24/epel-httpd24.repo
-      yum -y install httpd24.x86_64
-      EOH
-    end
+bash 'update_for_apache_2.4' do
+   user 'root'
+   cwd '/etc/yum.repos.d/'
+   code <<-EOH
+   wget http://repos.fedorapeople.org/repos/jkaluza/httpd24/epel-httpd24.repo
+   yum -y install httpd24.x86_64
+   EOH
 end
