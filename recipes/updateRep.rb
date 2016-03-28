8c11f3ea89e84ffce23b365add0d821ba45379cf
Ameesh Trikha <ameesh.trikha@gmail.com>
2016-03-28 19:10:26 +0000

    updateRep.rb deleted online with Bitbucket

--- recipes/updateRep.rb
+++
@@ -1,8 +0,0 @@
-bash 'update_for_apache_2.4' do
-   user 'root'
-   cwd '/etc/yum.repos.d/'
-   code <<-EOH
-   wget http://repos.fedorapeople.org/repos/jkaluza/httpd24/epel-httpd24.repo
-   yum -y install httpd24.x86_64
-   EOH
-end
